c7e26b7a858fad3feab9773b4630a1c21b73d2bc
Krishna <72639201+Krishna-74@users.noreply.github.com>
2021-04-17 19:47:05 +0530

    Add files via upload

---
+++ Averager.java
@@ -0,0 +1,19 @@
+
+public class Averager {
+	static int x,y;
+	static int average = (x+y)/2 ;
+	static void avg(int x , int y) {
+		double d=Double.parseDouble("average");
+	}
+	static void avg(int x , int y , int z) {
+		double d=Double.parseDouble("average");
+	}
+	static void avg(double x ,double y) {
+		double d=Double.parseDouble("average");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
