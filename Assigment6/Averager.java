9facc29075185cfa23d1fead5cea1b12be582752
Krishna <72639201+Krishna-74@users.noreply.github.com>
2021-04-17 20:07:16 +0530

    Create Averager.java

---
+++ Assigment6/Averager.java
@@ -0,0 +1,25 @@
+public class Averager {
+
+	int nums = 20;
+	static int staticNum = 40; 
+	
+	public static double avg(int a, int b) {
+		double average = (double) (a+b)/2;
+		return average;
+	}
+	
+	public static double avg(int a, int b, int c) {
+		double average = (double) (a+b+c)/3;
+		return average;
+	}
+	
+	public static double avg(double a, double b) {
+		double average = (a+b)/2;
+		return average;
+	}	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
