05de2e31fb6ec584dad42acf04080735682f6a4a
Krishna <72639201+Krishna-74@users.noreply.github.com>
2021-04-21 20:47:30 +0530

    Add

--- Day6/Assignment/one/ObjectOutputTreamOne.java
+++ Day6/Assignment/one/ObjectOutputTreamOne.java
@@ -6,10 +6,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-public class ObjectOutputStreamOneBatsMan {
+public class ObjectOutputStreamOne {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		BatsMan b1 = new BatsMan(101, "Sachin", 150, 900, 5493987);
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("records.dat"));
-		oos.writeObject(bt1);
+		oos.writeObject(b1);
 	}
 }
